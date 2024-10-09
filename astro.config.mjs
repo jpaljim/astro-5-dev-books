f22c0120258f98adb163e912c33397174bd80c58
Javier Palomo <javipalomo96@outlook.com>
2024-10-09 18:38:33 +0200

    Todo terminado

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,22 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from "astro/config";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  output: 'server',
+  integrations: [tailwind()],
+  env: {
+    schema: {
+      SHOW_BUY_BUTTON: envField.boolean({
+        context: "server",
+        access: "public",
+      }),
+      SCORE_API_ENDPOINT: envField.string({
+        context: "server",
+        access: "public",
+      }),
+    },
+  },
 });
